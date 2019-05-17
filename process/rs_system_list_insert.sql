84affc9a53be1667035bf28f30a569672d9a20c3
Nikolaj Potashnikov <consulting@yandex.ru>
2019-05-17 14:13:16 +0300

    add sorting

--- process/rs_system_list_insert.sql
+++ process/rs_system_list_insert.sql
@@ -7,21 +7,21 @@ create table logs.sys_list (
     ls_id String
 ) ENGINE = Join(any, inner, Module)
 ;;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('СКАТ-ДИР', 'СКАТ-ДИР', 'СКАТ-ДИР', 'skatdir');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('aoffense', 'АС «Мониторинг»', 'Единый реестр дел об АП', 'aoffense');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('АИС_РПТ', 'АИС РПТ', 'АИС РПТ', 'aisrpt');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('plan2019', 'АС «Мониторинг»', 'План проверок на 2019 год', 'plan2019');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_avto', 'АС «Мониторинг»', 'Статистическая отчетность -- Госавтодорнадзор', 'rostrans_avto');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('reestr', 'АС «Мониторинг»', 'Реестр поднадздорных субъектов', 'reestr');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('ЕРРД', 'ЕРРД', 'ЕРРД', 'errd');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans', 'АС «Мониторинг»', 'Сводная отчетность', 'rostrans');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_morrech', 'АС «Мониторинг»', 'Статистическая отчетность -- Госморречнадзор', 'rostrans_morrech');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_utb', 'АС «Мониторинг»', 'Статистическая отчетность -- УТБ', 'rostrans_utb');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_zhd', 'АС «Мониторинг»', 'Статистическая отчетность -- Госжелдорнадзор', 'rostrans_zhd');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_avia', 'АС «Мониторинг»', 'Статистическая отчетность -- Госавианадзор', 'rostrans_avia')    ;;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('PreventiveActionPlan', 'АС «Мониторинг»', 'Профилактические мероприятия', 'preventive_action_plan');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('plan2018', 'АС «Мониторинг»', 'План проверок на 2018 год', 'plan2018');;
-insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('incidentsZHD', 'АС «Мониторинг»', 'Учет пожаров на ЖДТ', 'incidents_zhd');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('АИС_РПТ', 'АИС РПТ', 'АИС РПТ', 'a010_aisrpt');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_avia', 'АС «Мониторинг»', 'Статистическая отчетность -- Госавианадзор', 'a020010_rostrans_avia')    ;;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_avto', 'АС «Мониторинг»', 'Статистическая отчетность -- Госавтодорнадзор', 'a020020_rostrans_avto');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_zhd', 'АС «Мониторинг»', 'Статистическая отчетность -- Госжелдорнадзор', 'a020030_rostrans_zhd');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_morrech', 'АС «Мониторинг»', 'Статистическая отчетность -- Госморречнадзор', 'a020040_rostrans_morrech');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans_utb', 'АС «Мониторинг»', 'Статистическая отчетность -- УТБ', 'a020050_rostrans_utb');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('aoffense', 'АС «Мониторинг»', 'Единый реестр дел об АП', 'a020060_aoffense');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('rostrans', 'АС «Мониторинг»', 'Сводная отчетность', 'a020070_rostrans');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('plan2018', 'АС «Мониторинг»', 'План проверок на 2018 год', 'a020080_plan2018');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('plan2019', 'АС «Мониторинг»', 'План проверок на 2019 год', 'a020090_plan2019');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('reestr', 'АС «Мониторинг»', 'Реестр поднадздорных субъектов', 'a020100_reestr');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('PreventiveActionPlan', 'АС «Мониторинг»', 'Профилактические мероприятия', 'a020110_preventive_action_plan');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('incidentsZHD', 'АС «Мониторинг»', 'Учет пожаров на ЖДТ', 'a020120_incidents_zhd');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('ЕРРД', 'ЕРРД', 'ЕРРД', 'a030010_errd');;
+insert into logs.sys_list (Module, system_name, system_module, ls_id) values ('СКАТ-ДИР', 'СКАТ-ДИР', 'СКАТ-ДИР', 'a040010_skatdir');;
  
 
 
